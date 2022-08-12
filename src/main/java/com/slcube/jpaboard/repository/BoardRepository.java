e5ddfc8e58f534cdff9e32878fe468b20dbb534c
SLCube <kgm0096@naver.com>
2022-08-12 23:29:56 +0900

    [TDD] BoardRepository : save method 구현 및 테스트 코드 성공.

--- src/main/java/com/slcube/jpaboard/repository/BoardRepository.java
+++ src/main/java/com/slcube/jpaboard/repository/BoardRepository.java
@@ -1,8 +1,19 @@
 package com.slcube.jpaboard.repository;
 
+import com.slcube.jpaboard.domain.Board;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+
 @Repository
+@RequiredArgsConstructor
 public class BoardRepository {
 
+    private final EntityManager em;
+
+    public Long save(Board board) {
+        em.persist(board);
+        return board.getId();
+    }
 }
