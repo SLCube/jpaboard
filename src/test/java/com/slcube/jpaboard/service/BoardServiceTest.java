971835687fb10044363ecff6593dce0278da1a95
SLCube <kgm0096@naver.com>
2022-08-15 12:05:12 +0900

    [TDD] : 스프링에 의존적인 BoardService 테스트 코드 작성

---
+++ src/test/java/com/slcube/jpaboard/service/BoardServiceTest.java
@@ -0,0 +1,43 @@
+package com.slcube.jpaboard.service;
+
+import com.slcube.jpaboard.domain.Board;
+import com.slcube.jpaboard.repository.BoardRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@Transactional
+class BoardServiceTest {
+
+    @Autowired
+    BoardService boardService;
+
+    @Test
+    void 게시글_작성() {
+        //given
+        Board board = new Board();
+        board.setTitle("test board title");
+        board.setContent("test board content");
+        board.setWriter("test board writer");
+        board.setViewCount(0);
+        board.setDeleteYn("N");
+
+        //when
+        Long boardId = boardService.register(board);
+
+        //then
+        Board findBoard = boardService.findOne(boardId);
+        assertThat(findBoard.getTitle()).isEqualTo("test board title");
+        assertThat(findBoard.getContent()).isEqualTo("test board content");
+        assertThat(findBoard.getWriter()).isEqualTo("test board writer");
+    }
+}
