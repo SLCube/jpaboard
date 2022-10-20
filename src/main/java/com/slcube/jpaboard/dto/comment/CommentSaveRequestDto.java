e18f331cf854b69b7bd464550d9dceace3bf53ce
SLCube <kgm0096@naver.com>
2022-10-20 11:09:09 +0900

    CommentSaveRequestDto 생성자에 Builder annotation 적용

--- src/main/java/com/slcube/jpaboard/dto/comment/CommentSaveRequestDto.java
+++ src/main/java/com/slcube/jpaboard/dto/comment/CommentSaveRequestDto.java
@@ -1,6 +1,7 @@
 package com.slcube.jpaboard.dto.comment;
 
 import com.slcube.jpaboard.domain.Comment;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,13 @@ public class CommentSaveRequestDto {
     private Long boardId;
     private String author;
     private String content;
+
+    @Builder
+    public CommentSaveRequestDto(Long boardId, String author, String content) {
+        this.boardId = boardId;
+        this.author = author;
+        this.content = content;
+    }
 
     public Comment toEntity() {
         return Comment.builder()
