6aeb2d328cbc0d3535890ac76d742b2ce6a5b391
SLCube <kgm0096@naver.com>
2022-10-20 16:20:18 +0900

    간단한 BoardController 테스트 작성

--- src/test/java/com/slcube/jpaboard/controller/BoardControllerTest.java
+++ src/test/java/com/slcube/jpaboard/controller/BoardControllerTest.java
@@ -1,13 +1,15 @@
 package com.slcube.jpaboard.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.slcube.jpaboard.dto.board.BoardSaveRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,11 +19,22 @@ public class BoardControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void findByIdTest() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/boards")
-                        .contentType(APPLICATION_JSON)
-                )
+    void saveTest() throws Exception {
+        BoardSaveRequestDto requestDto = BoardSaveRequestDto.builder()
+                .title("test board title")
+                .content("test board content")
+                .author("test board author")
+                .build();
+
+        String jsonString = objectMapper.writeValueAsString(requestDto);
+
+        mvc.perform(post("/boards")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
                 .andExpect(status().isOk());
     }
 }
